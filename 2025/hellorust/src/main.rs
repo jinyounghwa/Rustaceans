0630570aaccd04e24e71c05efe69e75c47ba6734
jinyounghwa <timotolkie@gmail.com>
2025-04-19 13:17:29 +0900

    20240419

--- 2025/hellorust/src/main.rs
+++ 2025/hellorust/src/main.rs
@@ -1,21 +1,32 @@
-struct Circle {
-    radius: f64,
-}
+use std::collections::VecDeque;
 
-impl Circle {
-    fn area(&self) -> f64 {
-        3.14 * self.radius * self.radius
+fn check_remaining(input: &VecDeque<(&str, bool)>) {
+    for item in input {
+        if !item.1 {
+            println!("You must: {}", item.0);
+        }
     }
 }
 
-impl Circle {
-    fn circumference(&self) -> f64 {
-        2.0 * 3.14 * self.radius
+fn done(input: &mut VecDeque<(&str, bool)>) {
+    if let Some(mut task_done) = input.pop_back() { // 뒤쪽에서 꺼냅니다.
+        task_done.1 = true; // 완료 상태로 변경합니다.
+        input.push_front(task_done); // 완료된 작업을 앞쪽에 다시 추가합니다.
     }
 }
 
 fn main() {
-    let circle = Circle { radius: 10.0 };
-    println!("Area: {}", circle.area());
-    println!("Circumference: {}", circle.circumference());
+    let mut input = VecDeque::new();
+    let things_to_do = vec!["send email to customer", "call supplier", "write report"];
+    for thing in things_to_do {
+        input.push_back((thing, false)); // false는 완료되지 않은 상태를 나타냅니다.
+    }
+
+    done(&mut input); // 완료된 작업을 처리합니다.
+    done(&mut input); // 완료된 작업을 처리합니다.
+    check_remaining(&input); // 남은 작업을 확인합니다.
+
+    for task in &input {
+        println!("Task: {}, Done: {}", task.0, task.1);
+    }
 }
