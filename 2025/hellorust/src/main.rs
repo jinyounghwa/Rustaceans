511183e3d2ce4e7d20dcb35cb299655fb58d784d
jinyounghwa <timotolkie@daum.net>
2025-12-12 23:30:12 +0900

    update

--- 2025/hellorust/src/main.rs
+++ 2025/hellorust/src/main.rs
@@ -1,15 +1,11 @@
-macro_rules! check{ // 매크로 이름
-    ($input1:ident, $input2:expr) => {  
-        if $input1 > $input2 {
-            println!("{} is greater than {}", $input1, $input2);
-        } else {
-            println!("{} is less than {}", $input1, $input2);
-        }
-    };
-}
-
 fn main() {
-    let a = 10;
-    let b = 20;
-    check!(a, b);
+    let mut my_number = 8;
+    let num_ref = &mut my_number;
+    *num_ref += 10;
+    println!("my_number: {}", my_number);
+
+    let second_number = 800;
+    let triple_reference = &&&second_number;
+    println!("Second_number = triple_reference? {}",
+    second_number == ***triple_reference);
 }
